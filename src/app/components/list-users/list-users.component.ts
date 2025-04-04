9c23da2cb7bc6d061134e119105762cd7d82fe41
Jhon <yojhon@hotmail.es>
2025-04-03 22:56:37 -0500

    nuevos estilos

--- src/app/components/list-users/list-users.component.ts
+++ src/app/components/list-users/list-users.component.ts
@@ -4,23 +4,24 @@ import { userService } from '../../services/user.service';
 
 @Component({
   selector: 'app-list-users',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './list-users.component.html',
-  styleUrl: './list-users.component.css'
+  styleUrls: ['./list-users.component.css']
 })
-export class ListUsersComponent implements OnInit{
-
-  /**
-   *
-   */
-  users : any[] = [];
+export class ListUsersComponent implements OnInit {
+  users: any[] = [];
+  colors: string[] = ["#ff0000", "#ff6800", "#00ff1b", "#f6ff00", "#00c9ff", "#ff00e7"];
 
   constructor(private userService: userService) {}
 
   ngOnInit(): void {
-      this.userService.getAllUsers().subscribe(
-        (response)=>{
-        this.users = response.users; 
-   })
- }
+    this.userService.getAllUsers().subscribe((response) => {
+      this.users = response.users;
+    });
+  }
+
+  getColor(index: number): string {
+    return this.colors[index % this.colors.length];
+  }
 }
